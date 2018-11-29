c677daa1e2f336615fe32cf7cd757859f59675cb
Josh Price <joshua.price88@gmail.com>
2018-11-28 23:33:57 -0600

    commiting Price-PrimitiveTypes.js

---
+++ week-1/joshprice-exersise1.3.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Josh Price
+; Date: 28 November 2018
+; Modified By: Josh Price
+; Description: This program demonstrates the
+; use of the Node.js Debugger
+;===========================================
+*/ 
+
+console.log('Hello World, my name is Josh Price');
