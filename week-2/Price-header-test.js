72dec90c500b9005e3210ca9909c6013eb151e61
Josh Price <joshua.price88@gmail.com>
2018-12-07 19:38:22 -0600

    Added comments and header text to Price-header-test.js

--- week-2/Price-header-test.js
+++ week-2/Price-header-test.js
@@ -1,3 +1,15 @@
+/*
+============================================
+; Title:  Price-header-test.js
+; Author: Josh Price
+; Date:   7 December 2018
+; Modified By: Josh Price
+; Description: Displays a formatted header
+;===========================================
+*/
+
+//Variable to pull in header file
 const header = require('../Price-header');
 
+//Console.log to display formatted header
 console.log(header.display("Josh", "Price", "Exercise 2.2"));
